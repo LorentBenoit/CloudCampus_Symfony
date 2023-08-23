29afad1b322de0477251fcd546c0624562c3dd3a
LorentBenoit <85104816+LorentBenoit@users.noreply.github.com>
2023-08-23 13:30:04 +0200

    Activité 11-2

--- src/Service/Calculator.php
+++ src/Service/Calculator.php
@@ -4,24 +4,21 @@ namespace App\Service;
 
 class Calculator
 {
+
     public function getTotalHT($products)
     {
         $totalHT = 0;
 
         foreach ($products as $product) {
-            $totalHT += $product['price'] * $product['quantity'];
+            $totalHT += $product['product']->$price * $product['quantity'];
         }
 
         return $totalHT;
     }
 
-    public function getTotalTTC($products, $tva)
+    public function getTotalTTC($totalHT, $tva)
     {
-        $totalTTC = 0;
-
-        foreach ($products as $product) {
-            $totalTTC += $product['price'] * $product['quantity'] * (1 + $tva / 100);
-        }
+        $totalTTC = $totalHT * (1 + $tva / 100);
 
         return $totalTTC;
     }
--- tests/Service/CalculatorTest.php
+++ tests/Service/CalculatorTest.php
@@ -7,42 +7,46 @@ use PHPUnit\Framework\TestCase;
 
 class CalculatorTest extends TestCase
 {
+
+    public function getProducts()
+    {
+        return [
+            [
+                'product' => $this->createProduct("Ballon rouge", 10),
+                'quantity' => 2
+            ],
+            [
+                'product' => $this->createProduct("Ballon bleu", 15),
+                'quantity' => 3
+            ]
+        ];
+    }
+
+    public function createProduct($name, $price)
+    {
+        return ((new Product())
+        ->setName($name)
+        ->setPrice($price));
+    }
+
     public function testGetTotalHT()
     {
         $calculator = new Calculator();
 
-        $products = [
-            ['quantity' => 2, 'price' => 10],
-            ['quantity' => 3, 'price' => 15],
-        ];
+        $products = $this->getProducts();
 
-        $expectedTotalHT = 10 * 2 + 15 * 3;
-
-        $this->assertEquals($expectedTotalHT, $calculator->getTotalHT($products));
+        $this->assertEquals(65, $calculator->getTotalHT($products));
     }
 
     public function testGetTotalTTC()
     {
         $calculator = new Calculator();
 
-        $products = [
-            ['quantity' => 2, 'price' => 10],
-            ['quantity' => 3, 'price' => 15],
-        ];
+        $totalHT = 65;
 
         $tva = 20;
 
-        $expectedTotalTTC = (10 * 2 + 15 * 3) * (1 + $tva / 100);
-
-        $this->assertEquals($expectedTotalTTC, $calculator->getTotalTTC($products, $tva));
-    }
-
-    private function createProduct($price)
-    {
-        $product = new \stdClass();
-        $product->price = $price;
-
-        return $product;
+        $this->assertEquals(78, $calculator->getTotalTTC($totalHT, $tva));
     }
 }
 
